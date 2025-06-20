c23aa18a250b4e2bd79c9d9fb8b985ca6bd97994
Moseley, Jake <jakemoseley@my.unt.edu>
2025-06-20 18:24:03 -0500

    Added endian_swap functionality for 32-bit int

--- endian.c
+++ endian.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 uint32_t endian_swap(uint32 n) {
-  return ((n >> 24) & 0x000000FF) | // Moves B3 (MSB) to B0 (LSB)
+  return ((n >> 24) & 0x000000FF) | // Moves B3 (MSB) to  B0 (LSB)
          ((n >> 8) & 0x0000FF00) | // Moves B2 to B1
          ((n << 8) & 0x00FF0000) | // Moves B1 to B2
          ((n << 24) & 0xFF000000); // Moves B0 (LSB) to B3 (MSB)
