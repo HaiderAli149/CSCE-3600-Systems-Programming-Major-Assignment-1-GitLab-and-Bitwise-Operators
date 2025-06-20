982933a82e5c79e195fe1975a7d310fb62189186
HaiderAli149 <haideralibeg.2004@gmail.com>
2025-06-19 22:24:51 -0500

    Implemented clz.c function

---
+++ clz.c
@@ -0,0 +1,22 @@
+// count leading zeros in a binary number
+#include "major1.h"
+
+int count_leading_zeros(uint32_t n) {
+
+    int count = 0;
+
+    for(int i = 31; i >=0; i--)
+    {
+        if((n >> i) & 1) // Shift n right by i and check if the last but is 1. 
+        {
+            break; // Stop counting when we find the first 1
+        }
+        else
+        {
+            count++; // Increment count for each leading zero
+        }
+    }
+
+    return count;
+
+}
---
+++ major1.h
@@ -0,0 +1,17 @@
+#ifndef MAJOR1_H
+#define MAJOR1_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+int count_leading_zeros(uint32_t n);
+uint32_t endian_swap(uint32_t n);
+uint32_t rotate(uint32_t n, int k);
+int parity(uint32_t n);
+
+
+#endif // MAJOR1_H
+
+
+// End of file: major1.h
+// This header file defines the function prototype for count_leading_zeros, endian_swap, rotate, and parity functions.
