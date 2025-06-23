aed36d6c80521b69a69bdcbc51f793304120a261
HaiderAli149 <haideralibeg.2004@gmail.com>
2025-06-22 21:56:35 -0500

    test

---
+++ endian.c
@@ -0,0 +1,9 @@
+#include "major1.h"
+#include <stdint.h>
+
+uint32_t endian_swap(uint32_t n) {
+  return ((n >> 24) & 0x000000FF) | // Moves B3 (MSB) to  B0 (LSB)
+         ((n >> 8) & 0x0000FF00) | // Moves B2 to B1
+         ((n << 8) & 0x00FF0000) | // Moves B1 to B2
+         ((n << 24) & 0xFF000000); // Moves B0 (LSB) to B3 (MSB)
+  }
