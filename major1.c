6d41c8216bf3a08bb2d06b8cfc329e1462c2bf1e
Muhammad Beg <haideralibeg.2004@gmail.com>
2025-06-22 23:02:08 -0500

    Update major1.c

--- major1.c
+++ major1.c
@@ -49,23 +49,15 @@ int main() {
     // Ensure the number is within the valid range for a 32-bit
     switch (option) {
         case 1:
-            printf("Enter a 32-bit number (>= 1 and <= 4294967295, inclusively): ");
-            scanf("%u", &number);
             printf("The number of leading zeroes in %u is %d\n", number, count_leading_zeros(number));
             break;
         case 2:
-            printf("Enter a 32-bit number (>= 1 and <= 4294967295, inclusively): ");
-            scanf("%u", &number);
             printf("Endian swap of %u gives %d\n",number, endian_swap(number));
             break;
         case 3:
-            printf("Enter a 32-bit number (>= 1 and <= 4294967295, inclusively): ");
-            printf("Enter the number of positions to rotate-right the input (between 0 and 31, inclusively):");
-            printf("%u rotated by %d position gives us %u\n", number, rotate_amount, rotate(number, rotate_amount));
+            printf("%u rotated by %d position gives us %u\n", number, rotate_amount, rotateRight(number, rotate_amount));
             break;
         case 4:
-            printf("Enter a 32-bit number (>= 1 and <= 4294967295, inclusively): ");
-            scanf("%u", &number);
             printf("Parity of %u is %d\n",number, parity(number));
             break;
         default:
